a599f0a922e3c1b8ff615aff068c4802ed79e5fe
quangnguyenminh01072005-wq <quang.nguyenminh01072005@gmail.com>
2026-06-28 17:54:24 +0700

    Bài tuần 15: cài đặt Warshall kiểm tra liên thông

--- baitaptuan15.cpp
+++ baitaptuan15.cpp
@@ -9,6 +9,25 @@ const int INF = 1000000;
 
 // Cấu hình cặp dữ liệu: trọng số, đỉnh đến
 typedef pair<int, int> NodePair;
+string danhSachTinh[MAX_NODES] =
+{
+    "Ha Noi",
+    "Thai Nguyen",
+    "Bac Ninh",
+    "Bac Giang",
+    "Uong Bi",
+    "Hai Phong",
+    "Hai Duong",
+    "Hung Yen",
+    "Phu Ly",
+    "Hoa Binh",
+    "Son Tay"
+};
+struct Graph
+{
+    int totalNodes;
+    int matrix[MAX_NODES][MAX_NODES];
+};
 
 int main()
 {
