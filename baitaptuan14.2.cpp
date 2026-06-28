6c8bfc6565b5177ec15928bae835ec2d4f4e4b0b
quangnguyenminh01072005-wq <quang.nguyenminh01072005@gmail.com>
2026-06-28 17:54:23 +0700

    Bài tuần 14 phần 2: khai báo bản đồ giao thông

---
+++ baitaptuan14.2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <string>
+#include <queue>
+#include <iomanip>
+using namespace std;
+
+const int Sotinh_max = 11;    // khai bao 11 tinh theo de bai
+const string DSTinh[Sotinh_max] = {     // 11 tinh thoe bai
+    "HN", "TN", "BN", "BG", "UB", "HP", "HD", "HY", "PL", "HB", "ST"};
+// Các ký hiệu tương đương : Hà Nội, Thái Nguyên, Bắc Ninh,... theo đề bài
+
+struct Matran {   // khai báo
+    int Sotinhxet;
+    int matrix[Sotinh_max][Sotinh_max];
+};
+
+int main()
+{
+    return 0;
+}
