4e3080891003a5884803b48f7066d75f1ad37c81
quangnguyenminh01072005-wq <quang.nguyenminh01072005@gmail.com>
2026-06-28 17:54:23 +0700

    Bài tuần 14: cài đặt quay trái quay phải

--- baitaptuan14.cpp
+++ baitaptuan14.cpp
@@ -30,6 +30,35 @@ Node* createNode(int x) {
     return p;
 }
 
+// Quay phải
+Node* rightRotate(Node *y) {
+    Node *x = y->left;
+    Node *T2 = x->right;
+    x->right = y;
+    y->left = T2;
+    y->height = maxx(height(y->left), height(y->right)) + 1;
+    x->height = maxx(height(x->left), height(x->right)) + 1;
+    return x;
+}
+
+// Quay trái
+Node* leftRotate(Node *x) {
+    Node *y = x->right;
+    Node *T2 = y->left;
+    y->left = x;
+    x->right = T2;
+    x->height = maxx(height(x->left), height(x->right)) + 1;
+    y->height = maxx(height(y->left), height(y->right)) + 1;
+    return y;
+}
+
+// Hệ số cân bằng
+int getBalance(Node *p) {
+    if (p == NULL)
+        return 0;
+    return height(p->left) - height(p->right);
+}
+
 int main()
 {
     return 0;
