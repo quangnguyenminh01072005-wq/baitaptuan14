32c8bba078840e9e77f4ea2a8c5eff32dc8dc507
quangnguyenminh01072005-wq <quang.nguyenminh01072005@gmail.com>
2026-06-28 17:54:20 +0700

    Bài tuần 6: tính dung lượng và tìm file nhỏ nhất

--- baitaptuan6.cpp
+++ baitaptuan6.cpp
@@ -16,3 +16,38 @@ struct LinkedList {
 void init(LinkedList &list) {
     list.head = NULL;
 }
+void insertFile(LinkedList &list, File f) {
+    Node* newNode = new Node{f, NULL};
+    if (list.head == NULL || f.time < list.head->data.time) {
+        newNode->next = list.head;
+        list.head = newNode;
+        return;
+    }
+    Node* cur = list.head;
+    while (cur->next != NULL && cur->next->data.time < f.time) {
+        cur = cur->next;
+    }
+    newNode->next = cur->next;
+    cur->next = newNode;
+}
+int totalSize(LinkedList list) {
+    int sum = 0;
+    Node* cur = list.head;
+    while (cur != NULL) {
+        sum += cur->data.size;
+        cur = cur->next;
+    }
+    return sum;
+}
+// tìm file nhỏ nhất
+Node* findMin(LinkedList list) {
+    Node* minNode = list.head;
+    Node* cur = list.head;
+    while (cur != NULL) {
+        if (cur->data.size < minNode->data.size) {
+            minNode = cur;
+        }
+        cur = cur->next;
+    }
+    return minNode;
+}
