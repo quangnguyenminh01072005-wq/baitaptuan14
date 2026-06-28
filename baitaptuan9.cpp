3f711210f99cacabaae91781be3bf5708c6f57c2
quangnguyenminh01072005-wq <quang.nguyenminh01072005@gmail.com>
2026-06-28 17:54:21 +0700

    Bài tuần 9: khai báo sinh viên và node danh sách

---
+++ baitaptuan9.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstring>
+using namespace std;
+struct Ngay{
+    int d,m,y;
+};
+struct SinhVien{
+    char ma[10], ten[50];
+    Ngay ns;
+};
+
+int main()
+{
+    return 0;
+}
