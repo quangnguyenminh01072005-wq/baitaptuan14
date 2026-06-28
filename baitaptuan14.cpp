fa7a91d608a6740e37a61dfb8f0351652613fc47
quangnguyenminh01072005-wq <quang.nguyenminh01072005@gmail.com>
2026-06-28 17:54:23 +0700

    Bài tuần 14: thêm chèn node vào AVL

--- baitaptuan14.cpp
+++ baitaptuan14.cpp
@@ -59,6 +59,51 @@ int getBalance(Node *p) {
     return height(p->left) - height(p->right);
 }
 
+// B2: Thêm nút vào AVL
+Node* insert(Node *root, int key) {
+    if (root == NULL)
+        return createNode(key);
+    if (key < root->key)
+        root->left = insert(root->left, key);
+    else if (key > root->key)
+        root->right = insert(root->right, key);
+    else
+        return root;
+    root->height = 1 + maxx(height(root->left),
+                            height(root->right));
+    int balance = getBalance(root);
+
+    // LL
+    if (balance > 1 && key < root->left->key)
+        return rightRotate(root);
+
+    // RR
+    if (balance < -1 && key > root->right->key)
+        return leftRotate(root);
+
+    // LR
+    if (balance > 1 && key > root->left->key) {
+        root->left = leftRotate(root->left);
+        return rightRotate(root);
+    }
+
+    // RL
+    if (balance < -1 && key < root->right->key) {
+        root->right = rightRotate(root->right);
+        return leftRotate(root);
+    }
+    return root;
+}
+
+// Duyệt NLR
+void preorder(Node *root) {
+    if (root != NULL) {
+        cout << root->key << " ";
+        preorder(root->left);
+        preorder(root->right);
+    }
+}
+
 int main()
 {
     return 0;
