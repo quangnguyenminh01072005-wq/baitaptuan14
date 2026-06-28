f0bdcd7b91f994954cb6dea9199451436e10c924
quangnguyenminh01072005-wq <quang.nguyenminh01072005@gmail.com>
2026-06-28 17:54:25 +0700

    Bài tuần 18: dựng khung đồ thị giao thông

---
+++ baitaptuan18.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <iomanip>
+#include <string>
+#include <vector>
+#include <queue>
+#include <functional>
+#include <algorithm>
+#include <utility>
+using namespace std;
+
+const int SO_TINH = 11;
+const int VO_CUC = 1000000000;
+
+const string tenTinh[SO_TINH] =
+{
+    "Ha Noi",
+    "Thai Nguyen",
+    "Bac Ninh",
+    "Bac Giang",
+    "Uong Bi",
+    "Hai Phong",
+    "Hai Duong",
+    "Hung Yen",
+    "Phu Ly",
+    "Hoa Binh",
+    "Son Tay"
+};
+
+int main()
+{
+    return 0;
+}
