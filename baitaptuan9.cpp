38deb383b8b3d8b95ee32383e5696229b69f177b
quangnguyenminh01072005-wq <quang.nguyenminh01072005@gmail.com>
2026-06-28 17:54:21 +0700

    Bài tuần 9: thêm nhập sinh viên và chèn danh sách

--- baitaptuan9.cpp
+++ baitaptuan9.cpp
@@ -8,6 +8,29 @@ struct SinhVien{
     char ma[10], ten[50];
     Ngay ns;
 };
+struct Node{
+    SinhVien data;
+    Node *next;
+};
+
+//================= TẠO NODE =================
+Node* taoNode(SinhVien x){
+    Node* p = new Node;
+    p->data = x;
+    p->next = NULL;
+    return p;
+}
+
+//================= NHẬP =================
+void nhapSV(SinhVien &x){
+    cin.ignore();
+    cout<<"Ma: ";
+    cin.getline(x.ma,10);
+    cout<<"Ten: ";
+    cin.getline(x.ten,50);
+    cout<<"Ngay sinh: ";
+    cin>>x.ns.d>>x.ns.m>>x.ns.y;
+}
 
 int main()
 {
